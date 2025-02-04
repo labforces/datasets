ae355aec25be6ec036450892ace9384717df6c40
labforces <119479005+labforces@users.noreply.github.com>
2025-02-04 17:22:11 +0500

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,28 +6,28 @@ from datetime import datetime, timedelta
 
 st.set_page_config(layout="wide")
 
-@st.cache_data
-def load_data():
-    df = pd.read_parquet(file)
-    return df
+#@st.cache_data
+#def load_data():
+ #   df = pd.read_parquet(file)
+ #   return df
 
-@st.cache_data
-def load_data_returns():
-    df = pd.read_parquet(file_return)
-    return df
+#@st.cache_data
+#def load_data_returns():
+ #   df = pd.read_parquet(file_return)
+  #  return df
 
 
-@st.cache_data
-def load_data_withdraw():
-    df = pd.read_parquet(file_withdraw)
-    return df
+#@st.cache_data
+#def load_data_withdraw():
+ #   df = pd.read_parquet(file_withdraw)
+  #  return df
 
-file = r'C:\Users\l.Islomkhujaev\Desktop\Streamlit\january-september.parquet'
-file_return = r'C:\Users\l.Islomkhujaev\Desktop\Streamlit\january-september-returns.parquet'
-file_withdraw = r'C:\Users\l.Islomkhujaev\Desktop\Streamlit\january-september-withdraw.parquet'
-pdf_returns = load_data_returns()
-pdf_withdraw = load_data_withdraw()
-pdf = load_data()
+#file = r'C:\Users\l.Islomkhujaev\Desktop\Streamlit\january-september.parquet'
+#file_return = r'C:\Users\l.Islomkhujaev\Desktop\Streamlit\january-september-returns.parquet'
+#file_withdraw = r'C:\Users\l.Islomkhujaev\Desktop\Streamlit\january-september-withdraw.parquet'
+#pdf_returns = load_data_returns()
+#pdf_withdraw = load_data_withdraw()
+#pdf = load_data()
 
 
 # Создайте список страниц
@@ -41,8 +41,6 @@ selected_page = st.sidebar.selectbox("Выберите страницу", pages)
 if selected_page == "Промо Календарь":
     
     st.title('Промо :blue[Календарь]:')
-    
-    os.chdir(r"C:\Users\l.Islomkhujaev\Desktop\Streamlit")
 
     df = pd.read_excel('Промо календарь.xlsx')
     
